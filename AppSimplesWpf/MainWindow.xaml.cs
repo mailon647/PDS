ad20e87d9dec9e875386de9ee3c78f4e278dec25
mailon647 <84489234+mailon647@users.noreply.github.com>
2023-04-17 17:02:35 -0300

    Add files via upload

---
+++ AppSimplesWpf/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppSimplesWpf
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(tbValor1.Text);
+            double b = Convert.ToDouble(tbValor2.Text);
+            double t = a * b;
+
+            tbResultado.Text = a.ToString() +  " X "  + b.ToString() + " = " + t.ToString();
+        }
+    }
+}
